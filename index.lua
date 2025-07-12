5324d7bb4985555a03c5c1e655ef46a82f2a76be
razmiO <thomas.cuesta31@gmail.com>
2025-07-12 22:11:37 +0200

    feat: add some test content

--- index.lua
+++ index.lua
@@ -1,8 +1,52 @@
 local server = require("lib/server")
 local app = require("lib/app").new()
+local logger = require("lib/middleware/logger")
+local JSON = require("cjson")
+local HTTP400 = require("lib/http-exception/bad-request")
+local uv = require("luv")
 
-app:all("*", function(c)
-    return c:html("<h1>Hello World</h1>")
+local posts = {}
+
+app:use("*", logger())
+app:post("/post", function(c)
+    if not c.req.hasBody then return HTTP400(c) end
+    local body = JSON.decode(c.req.body)
+
+    posts[#posts + 1] = {
+        id = #posts + 1,
+        author = body.author,
+        content = body.content,
+    }
+
+    -- Return a success response
+    return c:json({
+        message = "Post added successfully",
+        post = body,
+    })
+end)
+
+app:get("/post", function(c)
+    return c:json(posts)
+end)
+
+
+app:get("/post/:id", function(c)
+    local id = tonumber(c.req:param("id"))
+    local post = posts[id]
+    if not post then
+        return c:json({ message = "Post " .. tostring(id) .. " not found" }, 404)
+    end
+    return c:json(post)
+end)
+
+app:get("/slow", function(c)
+    uv.timer_start(uv.new_timer(), 1000, 0, function()
+        return c:text("slow response")
+    end)
+end)
+
+app:get("/fast", function(c)
+    return c:text("fast response")
 end)
 
 server.new():start(app, {
